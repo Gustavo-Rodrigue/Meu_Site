db9ac81fac8c80a09516008ebfa2186f8cb71f37
Gustavo <gustavorsenai@gmail.com>
2025-03-25 11:43:26 -0300

    Meu Site

--- script.js
+++ script.js
@@ -68,3 +68,38 @@ const savedTheme = localStorage.getItem('theme');
 if (savedTheme){
     document.documentElement.classList.toggle('light', savedTheme === 'light');
 }
+
+// ===== Animação do titulo principal
+// Seleciona o elemento 'título' e define as variáveis para animação
+const titleElement = document.querySelector('#name');
+const text = "GUSTAVO";
+let index = 0;
+let isTyping = true;
+let currentColor = document.documentElement.classList.contains('light') ? 'black' : '#fff';
+
+// Função para animar o texto (digitando e apagando)
+function animateText(){
+    if(isTyping){
+        if(index < text.length){
+            titleElement.textContent = text.slice(0, index + 1);
+            index ++;
+        } else {
+            isTyping = false;
+        }
+    } else {
+        if (index > 1){
+            titleElement.textContent = text.slice(0, index -1);
+            index --;
+        } else {
+            isTyping = true;
+            // alterna a cor entre preto e laranja
+            currentColor = currentColor === (document.documentElement.classList.contains('light') ? 'black' : '#fff') ? '##543290' : (document.documentElement.classList.contains('light') ? 'black' : '#fff');
+            titleElement.style.color = currentColor;
+        }
+    }
+    setTimeout(animateText, 150);
+}
+
+// inicia a animação quando carregar a página
+document.addEventListener('DOMContentLoaded', animateText);
+updateTextColor();
