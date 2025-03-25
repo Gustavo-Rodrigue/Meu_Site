c8701e99e9c8dad44ed4a2e3f3cf58dea0589bd0
Gustavo <gustavorsenai@gmail.com>
2025-03-25 13:24:32 -0300

    Meu site

--- script.js
+++ script.js
@@ -93,7 +93,7 @@ function animateText(){
         } else {
             isTyping = true;
             // alterna a cor entre preto e laranja
-            currentColor = currentColor === (document.documentElement.classList.contains('light') ? 'black' : '#fff') ? '##543290' : (document.documentElement.classList.contains('light') ? 'black' : '#fff');
+            currentColor = currentColor === (document.documentElement.classList.contains('light') ? 'black' : '#fff') ? '#543290' : (document.documentElement.classList.contains('light') ? 'black' : '#fff');
             titleElement.style.color = currentColor;
         }
     }
@@ -103,3 +103,51 @@ function animateText(){
 // inicia a animação quando carregar a página
 document.addEventListener('DOMContentLoaded', animateText);
 updateTextColor();
+
+//  ================= ANIMAÇÂO DA SEÇÂO HOME =============
+// Seleciona a seção home e aplica uma animação de fade-in
+const homeSection = document.querySelector('#home');
+homeSection.style.opacity = '0';
+homeSection.style.transform = 'translateY(20px)';
+homeSection.style.transition = 'opacity 1s ease, transform 1s ease';
+
+setTimeout(()=> {
+    homeSection.style.opacity = '1'
+    homeSection.style.transform = 'translateY(0)';
+}, 100);
+
+//  ================= ANIMAÇÂO DAS SEÇÕES =============
+// Seleciona todas as seções e aplica animações de entrada
+const sections = document.querySelectorAll('section');
+
+sections.forEach((section, index) => { 
+    section.style.opacity = '0';
+    section.style.transition = 'opacity 1s, transform 1s';
+
+    // Aplica diferentes transformações com base no índice da seção
+    if (index !== 0){
+        if (index === 1) section.style.transform = 'translateY(100px)';
+        if (index === 2) section.style.transform = 'scale(0.8)';
+        if (index === 3) section.style.transform = 'rotateY(90deg)';
+    }
+});
+
+// Observer para animar as seções ao rolar a página
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.style.opacity = '1';
+            entry.target.style.transform = 'none';
+        }
+    });
+});
+
+// Observa cada seção para aplicar a animação
+sections.forEach((section) => observer.observe(section));
+
+// =============== BOTÂO DE VOLTAR AO TOPO =====================
+// Adiciona um evento de clique ao botão de voltar ao topo
+document.querySelector('.top a').addEventListener('click', (e) => {
+    e.preventDefault();
+    window.scrollTo({ top: 0, behavior: 'smooth'}); // Rola suavemmente o topo da página
+})
